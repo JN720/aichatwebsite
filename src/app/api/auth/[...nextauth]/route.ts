772d63eb3e8cd68cea7a9451dce98264c1a0a61d
JN720 <112584623+JN720@users.noreply.github.com>
2023-09-22 13:23:57 -0700

    added sign in

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,54 +1,45 @@
 import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import CredentialsProvider from 'next-auth/providers/credentials'
+import User from 'next-auth'
+
+import { sql } from '@vercel/postgres';
 
 export const handler = NextAuth({
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID ?? '',
-      clientSecret: process.env.GITHUB_SECRET ?? ''
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-          email: {
-              label: "Email",
-              type: "text",
-              placeholder: "Enter email",
-          },
-          password: {
-              label: "Password",
-              type: "password",
-              placeholder: "Enter Password",
-          },
-      },
-      async authorize(credentials, req) {
-        const email = credentials?.email;
-        const password = credentials?.password;
-        const res = await fetch("http://localhost:3000/api/login", {
-          method: "POST",
-          headers: {"Content-Type": "application/json"},
-          body: JSON.stringify({email, password}),
-        });
-        const user = await res.json();
-        if (res.ok && user) {
-            return user;
-        } else return null;
-      }
-    }
-    )],
-  callbacks: {
-    async jwt({ token, user }) {
-        return { ...token, ...user };
-    },
-    async session({ session, token, user }) {
-        // Send properties to the client, like an access_token from a provider.
-        session.user = token;
-        return session;
-    }},
-  pages: {
-    signIn: '/auth/signin',
-  }
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID ?? '',
+            clientSecret: process.env.GITHUB_SECRET ?? ''
+        }),
+        CredentialsProvider({
+            name: "Email",
+            credentials: {
+                email: {
+                    label: "Email",
+                    type: "text",
+                    placeholder: "Enter email",
+                },
+                password: {
+                    label: "Password",
+                    type: "password",
+                    placeholder: "Enter Password",
+                },
+            },
+            async authorize(credentials) {
+                const email = credentials?.email;
+                const password = credentials?.password;
+                const { rows } = await sql`SELECT name, password FROM Users WHERE email = ${email} AND auth = 0;`;
+                try {
+                    if (rows[0].password != password) {
+                        return;
+                    }
+                } catch(e) {
+                    return;
+                }
+                const user = {email: rows[0].email, name: rows[0].name, id: '1'}
+                return user;
+            }
+      })]
 })
 
 export {handler as GET, handler as POST};
