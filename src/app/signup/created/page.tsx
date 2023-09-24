3a8ff783cd7c7a9a5b5aff6a12d663a2fb5a7c22
JN720 <112584623+JN720@users.noreply.github.com>
2023-09-23 19:35:00 -0700

    styling

--- src/app/signup/created/page.tsx
+++ src/app/signup/created/page.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link'
 
 export default function Created() {
-    return <div className = "w-full h-full">
-        <h1 className = "">Your account has been created</h1>
-        <Link className = "m-2 p-2 text-s text-blue-600 hover:text-blue-400" href = "/api/auth/signin"></Link>
+    return <div className = "flex flex-col items-center justify-center w-full min-h-screen">
+        <p className = "py-6 text-4xl text-center">Your account has been created!</p>
+        <Link className = "text-4xl text-center text-blue-600 hover:text-blue-400" href = "/api/auth/signin"> Click to go to Sign In</Link>
     </div>
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -15,23 +15,24 @@ async function hash(inputString: string): Promise<string> {
 
 function Submit(email: string, name: string, password: string, confPass: string) {
     if (!email || !name || !password || !confPass) {
-        return null;
+        return <button className = "p-4 mt-3 text-2xl rounded-xl align-middle text-gray-400 bg-gray-700" value = "Disabled">Sign Up</button>;
     }
+    const errorStyle = 'p-4 mt-3 rounded-xl text-2xl bg-red-600'
     const emailre = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const namere = /^[a-zA-Z0-9_]+$/
     if (!emailre.test(email.toLowerCase())) {
-        return <p className = "bg-red-800">Invalid Email</p>
+        return <p className = {errorStyle}>Invalid Email</p>
     }
     if (!namere.test(name) || name.length < 3) {
-        return <p className = "bg-red-800">Invalid Username: Must only contain alphanumeric characters and underscores and be at least 3 characters long</p>
+        return <p className = {errorStyle}>Invalid Username: Must only contain alphanumeric characters and underscores and be at least 3 characters long</p>
     }
     if (password.length < 4) {
-        return <p className = "bg-red-800">Passwords must be at least 4 characters long</p>
+        return <p className = {errorStyle}>Passwords must be at least 4 characters long</p>
     }
     if (password != confPass) {
-        return <p className = "bg-red-800">Passwords Unmatched</p>
+        return <p className = {errorStyle}>Passwords Unmatched</p>
     }
-    return <button className = "m-2 p-2 text-2xl rounded-xl align-middle bg-blue-900 hover:bg-blue-700" onClick = {() => {Result(email, name, hash(password))}}>Sign Up</button>
+    return <button className = "p-4 mt-3 text-2xl rounded-xl align-middle bg-blue-900 hover:bg-blue-700" onClick = {() => {Result(email, name, hash(password))}}>Sign Up</button>
 }
 
 async function Result(email: string, name: string, password: Promise<string>) {
@@ -47,23 +48,23 @@ async function Result(email: string, name: string, password: Promise<string>) {
 }
 
 export default function Signup() {
-    const labelFormat = 'p-6 text-xl';
-    const fieldFormat = 'm-4 p-2 bg-slate-800';
+    const labelFormat = 'p-3 text-xl';
+    const fieldFormat = 'm-2 mt-0 p-1 bg-slate-800';
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
     const [confPass, setConfPass] = useState('');
-    return <div className = "p-24 w-screen h-screen content-middle place-items-center bg-slate-900">
-        <h1 className = "p-6 text-2xl">Create Account</h1>
-        <label htmlFor = "email" className = {labelFormat}>Email</label><br/>
-        <input id = "email" className = {fieldFormat} type = "text" value = {email} onChange = {(e) => {setEmail(e.target.value)}}/><br/>
-        <label htmlFor = "name" className = {labelFormat}>Username</label><br/>
-        <input id = "name" className = {fieldFormat} type = "text" value = {name} onChange = {(e) => {setName(e.target.value)}}/><br/>
-        <label htmlFor = "password" className = {labelFormat}>Password</label><br/>
-        <input id = "password" className = {fieldFormat} type = "password" value = {password} onChange = {(e) => {setPassword(e.target.value)}}/><br/>
-        <label htmlFor = "confpass" className = {labelFormat}>Confirm Password</label><br/>
-        <input id = "confpass" className = {fieldFormat} type = "password" value = {confPass} onChange = {(e) => {setConfPass(e.target.value)}}/><br/>
+    return <div className = "flex flex-col p-24 w-screen h-screen items-center justify-center bg-slate-900">
+        <h1 className = "p-6 text-4xl">Create Account</h1>
+        <label htmlFor = "email" className = {labelFormat}>Email</label>
+        <input id = "email" className = {fieldFormat} type = "text" value = {email} onChange = {(e) => {setEmail(e.target.value)}}/>
+        <label htmlFor = "name" className = {labelFormat}>Username</label>
+        <input id = "name" className = {fieldFormat} type = "text" value = {name} onChange = {(e) => {setName(e.target.value)}}/>
+        <label htmlFor = "password" className = {labelFormat}>Password</label>
+        <input id = "password" className = {fieldFormat} type = "password" value = {password} onChange = {(e) => {setPassword(e.target.value)}}/>
+        <label htmlFor = "confpass" className = {labelFormat}>Confirm Password</label>
+        <input id = "confpass" className = {fieldFormat} type = "password" value = {confPass} onChange = {(e) => {setConfPass(e.target.value)}}/>
         {Submit(email, name, password, confPass)}
-        <Link className = "m-2 p-2 text-s text-blue-600 hover:text-blue-400" href = "/api/auth/signin">If you want to sign up using a provider such as GitHub, <b>Sign In</b> instead</Link>
+        <Link className = "m-2 p-2 text-s text-blue-500 hover:text-blue-300" href = "/api/auth/signin">If you want to sign up using a provider such as GitHub, <b>Sign In</b> instead</Link>
     </div>
 }
