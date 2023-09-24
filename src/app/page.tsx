0a3208742f5380b2098b9fbdd65d461e31bbc8ce
JN720 <112584623+JN720@users.noreply.github.com>
2023-09-23 19:48:20 -0700

    created simple home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link"
+
 export default function Home() {
-    return <h1>lo</h1>
+    return <>
+        <main className = "flex flex-col p-24 w-screen h-screen items-center justify-center">
+            <h1 className = "p-6 text-center text-5xl">Welcome to the Jsan Website</h1>
+            <h2 className = "p-6 text-center text-3xl text-slate-300">Named in Honor of Jsan</h2>
+            <Link className = "mt-3 p-6 text-center rounded-2xl text-4xl bg-blue-600 hover:bg-blue-400" href = "/chat">Try it Out</Link>
+        </main>
+    </>
 }
