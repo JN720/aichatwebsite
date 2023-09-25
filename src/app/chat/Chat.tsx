e04f12f07996df9aa4aa2002a3f6098db1ac1a5c
JN720 <112584623+JN720@users.noreply.github.com>
2023-09-24 18:19:48 -0700

    implemented non-authenticated chat

--- src/app/chat/Chat.tsx
+++ src/app/chat/Chat.tsx
@@ -1,62 +1,62 @@
 'use client';
 
+import axios from 'axios';
 import Chats from './chatClass';
 import Title from './Title';
-import { useSession } from 'next-auth/react';
+import { useSession, signIn } from 'next-auth/react';
 import { useState, useEffect } from 'react';
 
 export default function Chat() {
     const { data, status } = useSession();
-    let chatInfo: Chats;
+    let chatInfo = new Chats();
+
     useEffect(() => {
         if (status == 'authenticated') {
-            fetch('/chat/chats', {method: 'POST',headers: {'Content-Type': 'application/json'}, body: JSON.stringify({ 
-                a: 'e'
+            fetch('/chat/chats', {method: 'GET', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({ 
+                email: data.user?.email
             })}).then((res: Response) => res.json()).then((res) => {
                 chatInfo = res;
             });
-        } else {
-            chatInfo = new Chats();
         }
     }, [])
 
     const [textInput, setTextInput] = useState('');
-    const [textStream, setTextStream] = useState('something text EOS more text ');
+    const [textStream, setTextStream] = useState('');
     const [title, setTitle] = useState('New Chat');
-    const [chats, setChats] = useState([]);
-  
-    const handleButtonClick = () => {
-        if (!textInput) return;
-        fetch('', {
-            method: 'POST',headers: {'Content-Type': 'application/json'}, body: JSON.stringify({ 
-                instruction: 'Instruction: Respond intelligently.', 
-                knowledge: 'Knowledge: You are helpful.',
-                dialog: textStream + textInput
-            })
-        }).then((response) => response.json()).then((data: any) => {
-            setTextStream(textStream + data.message + ' EOS ');
-        }).catch((error) => {
-            console.error('Error:', error);
-        });
+    const [chatNames, setChatNames] = useState([]);
+
+    async function handleMessage(e: any) {
+        e.preventDefault();
+        if (!textInput) {
+            return;
+        }
+        const chat = textStream + textInput + ' EOS ';
+        setTextStream(chat + ' EOS ...');
+        setTextInput('');
+        const res = await axios.post('/chat/chats', {status: status, text: textStream + textInput + ' EOS '})
+        const newMsg: string = res.data.message;
+        setTextStream(chat + newMsg + ' EOS ');
     };
   
     return <>
         {Title(title)}
         <div className = "fixed top-1/6 left-0 w-2/12 h-full bg-slate-600">
-            {chats.map((chat: string) => {
+            {status == 'authenticated' ? chatNames.map((chat: string) => {
                 return <button className = "px-2 py-6 text-xl w-full text-start bg-slate-700 hover:bg-slate-500" key = {crypto.randomUUID()} onClick = {(e) => {setTitle(chat)}}>{chat}</button>
-            })}
+            }) : <button className = "px-2 py-6 text-3xl w-full text-start bg-slate-700 hover:bg-slate-500" onClick = {() => {signIn()}}>Sign in to save and store multiple chats!</button>}
         </div>
-        <div className = "flex flex-col items-center justify-start overflow-y-scroll" style = {{height: '65vh'}}>
+        <div className = "flex flex-col items-center justify-items-end w-full overflow-y-scroll" style = {{height: '55vh'}}>
             {textStream.split(' EOS ').map((text) => {
-                return <div className = "m-4 w-7/12 rounded-xl bg-slate-600" key = {crypto.randomUUID()}>
-                    <p className = "p-4 text-xl">{text}</p>
-                </div>
+                if (text) {
+                    return <div className = "m-4 w-7/12 rounded-xl bg-slate-600" key = {crypto.randomUUID()}>
+                        <p className = "p-4 text-xl">{text}</p>
+                    </div>
+                }
             })}
         </div>
         <div className = "flex flex-col items-center justify-end">
-            <textarea className = "m-3 p-1 text-left text-2xl w-1/3 h-40 rounded-md bg-blue-800 resize-none" rows = {3} placeholder = "Enter text" value = {textInput} onChange = {(e) => {setTextInput(e.target.value)}}/>
-            <button className = "m-1 p-3 text-center text-3xl w-48 rounded-md bg-lime-800 hover:bg-lime-500" onClick = {(handleButtonClick)}>Send</button>
+            <textarea className = "m-3 p-1 text-left text-2xl w-1/3 h-40 rounded-md bg-blue-950 resize-none" rows = {3} placeholder = "Enter text" value = {textInput} onChange = {(e) => {setTextInput(e.target.value)}}/>
+            <button className = "m-1 p-3 text-center text-3xl w-48 rounded-md bg-lime-800 hover:bg-lime-500" onClick = {(e) => handleMessage(e)}>Send</button>
         </div>
     </>
   }
--- src/app/chat/chats/route.ts
+++ src/app/chat/chats/route.ts
@@ -1,6 +1,22 @@
+import axios from 'axios';
 import { kv } from "@vercel/kv";
-import { sql } from "@vercel/postgres";
+import { db } from "@vercel/postgres";
+import { NextResponse } from "next/server";
 
-export async function POST() {
-    return '';
+//const client = db.connect();
+
+export async function POST(req: Request) {
+    const request = await req.json();
+    const text = await request.text;
+    const form = new FormData();
+    form.append('instruction', 'Instruction: Respond intelligently.');
+    form.append('knowledge', 'Knowledge: You are helpful.');
+    form.append('dialog', text);
+    const msg = await axios.post(process.env.GEN_URL ?? 'http://localhost:8000', form)
+    console.log(msg.data.message)
+    return NextResponse.json({message: msg.data.message});
+}
+
+export async function GET() {
+
 }
